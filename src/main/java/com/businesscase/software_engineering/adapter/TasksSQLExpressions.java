8bbfcd3149a7038c3ee47feafd5e5ec6d0da2edc
gersin <gersonfernandesribeiro48@gmail.com>
2025-02-21 12:42:32 -0300

    Consultas SQL

--- src/main/java/com/businesscase/software_engineering/adapter/TasksSQLExpressions.java
+++ src/main/java/com/businesscase/software_engineering/adapter/TasksSQLExpressions.java
@@ -6,31 +6,102 @@ public class TasksSQLExpressions {
             ordenacao = "DESC";
 
         return """
-                    SELECT 
-                        tasks.id_task, 
-                        tasks.title_task, 
-                        tasks.description_task, 
-                        tasks.status_task, 
-                        tasks.data_task, 
-                        tasks.data_modificacao_task 
+                SELECT
+                    tasks.id_task,
+                    tasks.title_task,
+                    tasks.description_task,
+                    tasks.status_task,
+                    tasks.data_task,
+                    tasks.data_modificacao_task
                     FROM tasks
-                    ORDER BY tasks.id_task """ + ' ' + ordenacao + ' ' + """
+                ORDER BY tasks.id_task""" + ' ' + ordenacao + ' ' + """
                     LIMIT :limite OFFSET :paginaAtual;
                 """;
     }
-    public static String sqlFindAll() {
-        return "select * from tasks";
+
+    public static String sqlCountTasks() {
+        return """
+                    SELECT COUNT(*) FROM tasks;
+                """;
     }
-    public static String sqlFindAll() {
-        return "select * from tasks";
+
+    public static String sqlFindById() {
+        return """
+                    SELECT
+                        tasks.id_task,
+                        tasks.title_task,
+                        tasks.description_task,
+                        tasks.status_task,
+                        tasks.data_task,
+                        tasks.data_modificacao_task
+                    FROM tasks
+                    WHERE tasks.id_task = :id_task
+                """;
     }
-    public static String sqlFindAll() {
-        return "select * from tasks";
+
+    public static String sqlFindByAllArgs(String parametro) {
+
+        return """
+                SELECT
+                	tasks.id_task,
+                	tasks.title_task,
+                	tasks.description_task,
+                	tasks.status_task,
+                	tasks.data_task,
+                	tasks.data_modificacao_task
+                FROM tasks
+                WHERE tasks.id_task LIKE""" + " '%" + parametro + "%'" + """
+                OR tasks.title_task LIKE""" + " '%" + parametro + "%'" + """
+                OR tasks.description_task LIKE""" + " '%" + parametro + "%'" + """
+                OR tasks.status_task LIKE""" + " '%" + parametro + "%'" + """
+                OR tasks.data_task LIKE""" + " '%" + parametro + "%'" + """
+                OR tasks.data_modificacao_task LIKE""" + " '%" + parametro + "%';" + """
+                """;
     }
-    public static String sqlFindAll() {
-        return "select * from tasks";
+
+    public static String sqlInsertTask() {
+
+        return """
+                INSERT INTO tasks (
+                    tasks.id_task,
+                    tasks.title_task,
+                    tasks.description_task,
+                    tasks.status_task,
+                    tasks.data_task
+                ) VALUES(
+                    :id_task,
+                    :title_task,
+                    :description_task,
+                    :status_task,
+                    :data_task
+                );
+                """;
     }
-    public static String sqlFindAll() {
-        return "select * from tasks";
+
+    public static String sqlUpdateTask() {
+
+        return """
+                UPDATE tasks
+                SET
+                    tasks.title_task = :title_task,
+                    tasks.description_task = :description_task,
+                    tasks.status_task = :status_task,
+                    tasks.data_task = :data_task,
+                    tasks.data_modificacao_task = :data_modificacao_task
+                WHERE tasks.id_task = :id_task;
+                """;
+    }
+
+    public static String sqlDeleteTask() {
+
+        return """
+                INSERT INTO tasks (
+                    tasks.id_task,
+                    tasks.title_task,
+                    tasks.description_task,
+                    tasks.status_task,
+                    tasks.data_task
+                WHERE tasks.id_task = :id_task
+                """;
     }
 }
